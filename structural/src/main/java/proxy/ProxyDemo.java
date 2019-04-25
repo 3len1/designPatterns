7d6721e7473681d48d3b65856b2f9c3e08539165
Eleni Vasilaki <evasilaki@cognity.gr>
2019-04-25 14:37:30 +0300

    ^^

---
+++ structural/src/main/java/proxy/ProxyDemo.java
@@ -0,0 +1,18 @@
+package proxy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by 3len1 on 4/24/2019.
+ */
+public class ProxyDemo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProxyDemo.class);
+
+    public static void main(String args[]) {
+        LOGGER.info("Proxy example with folders. Have fun to try it!");
+
+
+    }
+}
